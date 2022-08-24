2bc3b4f11dd1b5f2aa33781c032b1f0cb63c7927
Magdalena Michalak <mbawolik80@gmail.com>
2022-08-24 10:08:31 +0200

    modul 4

--- dingo/settings.py
+++ dingo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', "key testowy")
+SECRET_KEY = 'django-insecure-2f!9zs(6c(g@c0fw!3n7)c9@gluc1%8#%6=t)iadm*npqkug2n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
